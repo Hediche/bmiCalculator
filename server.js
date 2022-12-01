8f37bf1819cff31cd1e4dd34a5a2fc8869d15252
khadijah <halqigularip@gmail.com>
2022-12-01 01:10:57 +0100

    bmiCalculator edited

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ app.get("/",function(req,res){
     res.sendFile(__dirname + "/bmiCalculator.html");
 })
 
-app.post("/yourBMI",function(req,res){
-    var weight = Number(req.body.weight);
-    var height = Number(req.body.height);
-    res.send("Your BMI is:"+" "+ weight/(height^2) + ".");
-})
+app.post("/",function(req,res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var bmi = weight/(height^2);
+
+    res.send("Your BMI is:"+" "+ bmi + ".");
+});
 app.listen(3000,function(){
     console.log('Your server is running on port 3000!');
 });
